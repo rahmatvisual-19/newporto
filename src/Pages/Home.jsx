769c92fb671d0dcadf1bea67f5677d4932a2cca3
rahmatvisual-19 <rhma48475@gmail.com>
2026-05-08 15:03:02 +0700

    gif1

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -229,7 +229,7 @@ const Home = () => {
                     isHovering ? "scale-105" : "scale-100"
                   }`}>
                     <img
-                      src="Animation.gif"
+                      src="Animation1.gif"
                       alt="Developer Animation"
                       className={`w-full h-full object-contain transition-all duration-500 ${
                         isHovering 
